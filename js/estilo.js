547f13174a015d4fbd7371e769c7df92e9c5e600
Matheus Lopes <matheussouzalopes2006@gmail.com>
2024-08-12 21:16:30 -0300

    porti

---
+++ js/estilo.js
@@ -0,0 +1,18 @@
+const el_text = document.querySelector("#text");
+
+(function (){
+    const getText = el_text.textContent;
+    const textSlipt = getText.split('');
+    el_text.textContent="";
+    textSlipt.map((intem,line)=>{
+        setTimeout(()=>{
+
+            el_text.textContent += intem;
+        }, 100 * line);
+    })
+})();
+
+window.addEventListener("scroll", function(){
+    let header= document.querySelector('#header-1')
+    header.classList.toggle('rolagem',window.scrollY >0)
+})
